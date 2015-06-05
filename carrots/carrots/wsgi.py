6a35e0aafecc3b1d257f19fa8c55b02a8e377c6b
Balthazar Rouberol <br@imap.cc>
2015-06-05 19:42:59 +0200

    Rename project to enable collaboration

--- carrots/carrots/wsgi.py
+++ carrots/carrots/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for brweb project.
+WSGI config for carrots project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "brweb.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "carrots.settings")
 
 application = get_wsgi_application()
